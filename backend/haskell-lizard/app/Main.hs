73aa529b95e67a8f14c8792208fb64a70c08ae93
Jose Antonio Martinez Vidaurre <jbmw16@gmail.com>
2023-10-07 15:40:57 -0400

    lizard script done

--- backend/haskell-lizard/app/Main.hs
+++ backend/haskell-lizard/app/Main.hs
@@ -16,9 +16,12 @@ import System.Process
 import System.Directory
 import qualified Data.Vector as V
 
-file = "../../OneDrive/Desktop/WebScraping/output.csv"
+-- file = "../../OneDrive/Desktop/WebScraping/output.csv"
+file = "../../OneDrive/Desktop/WebScraping/old.csv"
 
 storeFolder = "gits"
+
+lizardExecutable = "lizard"
 
 data SRow = SRow    { user :: !String
                     , project :: !String
@@ -34,11 +37,39 @@ instance FromNamedRecord SRow where
 	parseNamedRecord x = SRow <$> x .: "User" <*> x .: "Project" <*> x .: "Repository URL" <*> x .: "About" <*> x .: "License" <*> x .: "Stars"
 instance DefaultOrdered SRow
 
--- getGits :: FilePath -> IO [String]
--- getGits = listDirectory 
+getGits :: FilePath -> IO [String]
+getGits = listDirectory 
 
 pullGits :: FilePath -> V.Vector (String, String) -> IO () 
 pullGits f l =  V.mapM_ (\(name,url) -> withCurrentDirectory f (runCommand ("git clone " ++ url) )) l
+
+-- lizardProcedure :: FilePath -> V.Vector (String, String) -> IO ()
+lizardProcedure f l = do
+    projectList <- getGits f
+    -- This call should return a list of all results of lizard compiled already
+    -- procedure should have the type of a [String]
+    results <- withCurrentDirectory f (procedure l)
+    V.mapM_ (\(_,x) -> putStrLn x )results
+    where 
+        procedure listP = V.mapM (perFolder) listP
+        -- IO Action to be run in each folder, wrapped as a tuple with the name of its project
+        -- perFolder :: (String, String) -> IO (a)
+        perFolder (name,url) =  do 
+            folders <- withCurrentDirectory name (releaseLizardParsed)
+            -- folders <- withCurrentDirectory name (readProcess "pwd" [] "") 
+            return (name, folders)
+
+releaseLizard = do 
+    (_, output, _) <- readProcessWithExitCode "lizard" [] []
+    return output
+
+releaseLizardParsed = do 
+    (_, output, _) <- readProcessWithExitCode "lizard" [] []
+    return $ head $ take 2 $ reverse $ lines output
+    
+
+
+
 
 
 readFromFile :: IO ()
@@ -46,9 +77,11 @@ readFromFile = do
 	f <- BL.readFile file 
 	case (decodeByName f) of 
 		Left err -> putStrLn err
-		Right (_, v) -> pullGits storeFolder $ (getListProjects v)
+		-- Right (_, v) -> print $ (getListProjects v)
+		Right (_, v) -> (lizardProcedure storeFolder) $ (getListProjects v)
     where
     	getListProjects v = V.map (\x -> (project x, repositoryUrl x) ) v
+
 
 
 
