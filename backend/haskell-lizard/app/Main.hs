92d9f6079819e13bc7a0290db94f746159bd0601
Jose Antonio Martinez Vidaurre <jbmw16@gmail.com>
2023-10-07 11:57:59 -0400

    pull git functionality

--- backend/haskell-lizard/app/Main.hs
+++ backend/haskell-lizard/app/Main.hs
@@ -12,40 +12,33 @@ import qualified Data.ByteString.Lazy as BL
 import System.IO
 import System.Exit
 import GHC.Generics
-import System.Command
+import System.Process
 import System.Directory
 import qualified Data.Vector as V
 
+file = "../../OneDrive/Desktop/WebScraping/output.csv"
 
-file = "output.csv"
 storeFolder = "gits"
 
-data SRow = SRow    { project :: !String
-				    , repositoryUrl :: !String
-					, about :: !String
+data SRow = SRow    { user :: !String
+                    , project :: !String
+		            , repositoryUrl :: !String
+                    , about :: !String
                     , licence :: !String
                     , starts :: !String
                     }
 	deriving (Generic, Eq, Show)
 
--- instance FromNamedRecord SRow where
-	-- parseNamedRecord x = fmap SRow (x .: "User/Project") 
-
 instance ToNamedRecord SRow
 instance FromNamedRecord SRow where
-	parseNamedRecord x = SRow <$> x .: "User/Project" <*> x .: "Repository URL" <*> x .: "About" <*> x .: "License" <*> x .: "Stars"
+	parseNamedRecord x = SRow <$> x .: "User" <*> x .: "Project" <*> x .: "Repository URL" <*> x .: "About" <*> x .: "License" <*> x .: "Stars"
 instance DefaultOrdered SRow
 
-getGits :: FilePath -> IO [String]
-getGits = listDirectory 
+-- getGits :: FilePath -> IO [String]
+-- getGits = listDirectory 
 
-pullGits :: FilePath -> [String] -> IO () 
-pullGits f l =  do 
-    mapM_ (\x -> print =<< getGits f) l
-
-pullGit :: String -> IO() 
-pullGit f = runCommand "git clone " ++ f
-
+pullGits :: FilePath -> V.Vector (String, String) -> IO () 
+pullGits f l =  V.mapM_ (\(name,url) -> withCurrentDirectory f (runCommand ("git clone " ++ url) )) l
 
 
 readFromFile :: IO ()
@@ -53,7 +46,9 @@ readFromFile = do
 	f <- BL.readFile file 
 	case (decodeByName f) of 
 		Left err -> putStrLn err
-		Right (_, v) -> V.forM_ v (\x -> print $ project x)
+		Right (_, v) -> pullGits storeFolder $ (getListProjects v)
+    where
+    	getListProjects v = V.map (\x -> (project x, repositoryUrl x) ) v
 
 
 
