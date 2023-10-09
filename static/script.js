9511d0b079a97ba464283965fc0d804b6b6a889f
alejandro0955 <33553049+alejandro0955@users.noreply.github.com>
2023-10-08 20:22:19 -0400

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,9 @@
+var result_container = document.getElementById("result_container")
+
+
+function add_info() {
+    div = document.createElement("div");
+    div.id = "result"
+    document.getElementById("result_container").appendChild(div);
+}
+
